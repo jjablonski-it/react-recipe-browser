c484131b69798c835e73dc76ddb984964af08fcb
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-03-28 22:27:42 +0100

    Fix not updating filenames attemp

--- src/components/Keywordss.js
+++ src/components/Keywordss.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
 // Components
-import Keyword from "./Keyword";
+import Keyword from "./Keywordd";
 
 export const Keywords = () => {
   const { keywords } = useContext(GlobalContext);

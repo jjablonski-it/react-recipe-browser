7b3606f2c73f74901c0c42b942d587c20680efb3
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-02-05 18:29:39 +0100

    moved essential components to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,33 @@
+import {
+  Container,
+  createMuiTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@material-ui/core";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import React from "react";
 import ContextProvider from "../context/Context";
 import "../styles/globals.css";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    background: {
+      default: "#242424",
+      // paper: "#a1a1a1",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContextProvider>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
     </ContextProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,13 @@
-import {
-  Container,
-  createMuiTheme,
-  CssBaseline,
-  Grid,
-  ThemeProvider,
-  Typography,
-} from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import Recipes from "../components/Recipes";
 import SearchBar from "../components/SearchBar";
 
-const theme = createMuiTheme({
-  palette: {
-    type: "dark",
-    background: {
-      default: "#242424",
-      // paper: "#a1a1a1",
-    },
-  },
-});
-
 export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container>
-        <Grid container justify="center">
-          <Typography variant="h1">Test</Typography>
-          <SearchBar />
-          <Recipes />
-        </Grid>
-      </Container>
-    </ThemeProvider>
+    <Grid container justify="center">
+      <Typography variant="h1">Test</Typography>
+      <SearchBar />
+      <Recipes />
+    </Grid>
   );
 }

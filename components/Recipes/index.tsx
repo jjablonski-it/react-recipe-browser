35449f559847e0c92e693e5509b31b5d9c2c288c
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-02-05 17:29:45 +0100

    added item staggering and hover animation

--- components/Recipes/index.tsx
+++ components/Recipes/index.tsx
@@ -1,7 +1,19 @@
 import { Box, Grid } from "@material-ui/core";
+import { motion, Variants } from "framer-motion";
 import React, { useContext } from "react";
 import { Context } from "../../context/Context";
 import RecipeComp from "./Recipe";
+
+const MotionGrid = motion.custom(Grid);
+
+const item: Variants = {
+  hidden: { opacity: 0, y: -20 },
+  show: (i) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.05 * i },
+  }),
+};
 
 const Recipes = () => {
   const { items } = useContext(Context);
@@ -11,13 +23,24 @@ const Recipes = () => {
       container
       justify="space-around"
       spacing={2}
-      alignItems="stretch"
       style={{ marginTop: "10px" }}
     >
       {items?.map((recipe, i) => (
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <RecipeComp key={i} recipe={recipe} />
-        </Grid>
+        <MotionGrid
+          key={i}
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          custom={i}
+          variants={item}
+          initial="hidden"
+          animate="show"
+          whileHover={{ scale: 0.95 }}
+        >
+          <RecipeComp recipe={recipe} />
+        </MotionGrid>
       ))}
     </Grid>
   );

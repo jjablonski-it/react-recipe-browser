fdeb856893f3f5150857dc9f0fc16c647338855a
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-02-05 17:41:45 +0100

    added dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,35 @@
-import { Container, Grid, Typography } from "@material-ui/core";
+import {
+  Container,
+  createMuiTheme,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
 import Recipes from "../components/Recipes";
 import SearchBar from "../components/SearchBar";
 
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    background: {
+      default: "#242424",
+      // paper: "#a1a1a1",
+    },
+  },
+});
+
 export default function Home() {
   return (
-    <Container>
-      <Grid container justify="center">
-        <Typography variant="h1">Test</Typography>
-        <SearchBar />
-        <Recipes />
-      </Grid>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Grid container justify="center">
+          <Typography variant="h1">Test</Typography>
+          <SearchBar />
+          <Recipes />
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }

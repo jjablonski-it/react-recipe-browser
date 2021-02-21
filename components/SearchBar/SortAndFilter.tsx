105afb8254048ab77468fd7015790bca50955c8e
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-02-21 17:07:39 +0100

    added pop menu for sorting

--- components/SearchBar/SortAndFilter.tsx
+++ components/SearchBar/SortAndFilter.tsx
@@ -1,18 +1,38 @@
-import { IconButton } from "@material-ui/core";
+import { IconButton, Menu, MenuItem } from "@material-ui/core";
 import { FilterList, Sort } from "@material-ui/icons";
 import React from "react";
+import { Recipe } from "../../context/types";
 
 interface Props {}
 
 const SortAndFilter = (props: Props) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleSortClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <IconButton>
         <FilterList />
       </IconButton>
-      <IconButton>
+      <IconButton onClick={handleSortClick}>
         <Sort />
       </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Option</MenuItem>
+        <MenuItem onClick={handleClose}>Option</MenuItem>
+        <MenuItem onClick={handleClose}>Option</MenuItem>
+      </Menu>
     </>
   );
 };
